c177af4d40c0776cdda7554bb60da5dfac66f541
Krystyna Szurmańska <kszurmanska@gmail.com>
2018-08-12 15:07:43 +0200

    Add project files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '80666632585-mms69266t3be32fisrcch8s8hj1hetup.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'z_Z_8kt7IIlRySSziO955oxu',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
